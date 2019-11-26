e3393532f389b9c922cf7c489456d2455154f26f
Joshua Delvaux <delvjd04@uwgb.edu>
2019-11-26 15:28:18 -0600

    Attempt #4

--- 2DSideScroller/Source.cpp
+++ 2DSideScroller/Source.cpp
@@ -7,64 +7,84 @@
 
 int main()
 {
+	//window dimensions
 	int width = 1280;
 	int height = 720;
 
+	//set up window & window title.
 	sf::RenderWindow window(sf::VideoMode(width, height), "2DSideScroller");
+	//limit framerate
 	window.setFramerateLimit(60);
 
+	//set up & initialize a view.
 	sf::View view = window.getDefaultView();
+	//set up viewport
 	view.setViewport(sf::FloatRect(0.f, 0.f, 1.f, 1.f));
-	view.zoom(0.5f);
+	//set zoom level
+	view.zoom(0.4f);
 
-	std::vector<sf::RectangleShape> levelTiles(200);
-
-	b2Vec2 gravity(0.0f, 60.f);
-	b2World world(gravity);
-
+	//set timeStep for Box2D physics simulation
 	float32 timeStep = 1.0f / 60.0f;
 
 	int32 velocityIterations = 6;
 	int32 positionIterations = 2;
 
+	b2Vec2 gravity(0.0f, 60.f);
+	b2World world(gravity);
+
+	//ground tile definition
 	b2BodyDef groundBodyDef;
 
+	//pointer to player object.
 	Player *newPlayer = new Player();
 
+	//uninitialized body for player
 	b2Body *newPlayerBody;
 
+	//initial position variables
 	float initX = 0;
 	float initY = 0;
 
+	//level structure
 	int level[10][40] =
 	{
 		{ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 2, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 	};
 
+	//storage for level.
 	std::vector<sf::RectangleShape> tiles(400);
 
+	//generate level
 	for (int c = 0; c < 40; c++)
 	{
 		for (int r = 0; r < 10; r++)
 		{
 			if (level[r][c] == 1)
 			{
-				groundBodyDef.position.Set(c * 50, r * 50);
+				groundBodyDef.position.Set(c * 50.f, r * 50.f);
 
 				b2Body* groundBody = world.CreateBody(&groundBodyDef);
 
 				b2PolygonShape groundBox;
 				groundBox.SetAsBox(25.0f, 25.0f);
-				groundBody->CreateFixture(&groundBox, 0.0f);
+				//groundBody->CreateFixture(&groundBox, 0.0f);
+
+				b2FixtureDef fixtureDef;
+
+				fixtureDef.shape = &groundBox;
+				fixtureDef.density = 0.0f;
+				fixtureDef.friction = 0.f;
+
+				groundBody->CreateFixture(&fixtureDef);
 
 				b2Vec2 groundPos = groundBody->GetPosition();
 
@@ -76,25 +96,32 @@ int main()
 			}
 			else if (level[r][c] == 2)
 			{
-				groundBodyDef.position.Set(c * 50, r * 50);
+				groundBodyDef.position.Set(c * 50.f, r * 50.f);
 
 				b2Body* groundBody = world.CreateBody(&groundBodyDef);
 
 				b2PolygonShape groundBox;
 				groundBox.SetAsBox(25.0f, 25.0f);
-				groundBody->CreateFixture(&groundBox, 0.0f);
+				//groundBody->CreateFixture(&groundBox, 0.0f);
+
+				b2FixtureDef fixtureDef;
+
+				fixtureDef.shape = &groundBox;
+				fixtureDef.density = 0.0f;
+				fixtureDef.friction = 0.f;
+
+				groundBody->CreateFixture(&fixtureDef);
 			}
 			else if (level[r][c] == 3)
 			{
-				initX = c * 50;
-				initY = r * 50;
+				initX = c * 50.f;
+				initY = r * 50.f;
 			}
 		}
 	}
 
+	//create a new body for our player.
 	newPlayerBody = newPlayer->createBody(&world, initX, initY);
-	
-	int jumpTimer = 0;
 
 	while (window.isOpen())
 	{
@@ -108,51 +135,56 @@ int main()
 
 		world.Step(timeStep, velocityIterations, positionIterations);
 
-		float desiredVelocity = 0;
-		
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		//keyboard inputs
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 		{
-			jumpTimer++;
-			if (jumpTimer < 10)
+			if (!newPlayerBody->GetContactList() == 0)
 			{
 				newPlayer->moveUp(newPlayerBody);
 			}
 		}
 
-		if (newPlayerBody->GetLinearVelocity().y == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		{
-			jumpTimer = 0;
-		}
+		//desired velocity
+		float desiredVelocity = 0;
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
 			desiredVelocity = -100;
 		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
 			desiredVelocity = 100;
 		}
 
+		//set up impulse for constant x-plane movement
 		float velChange = desiredVelocity - newPlayerBody->GetLinearVelocity().x;
 		float impulse = newPlayerBody->GetMass() * velChange;
 
+		//move the player
 		newPlayer->moveX(newPlayerBody, impulse);
 
+		//set view center
 		view.setCenter(newPlayerBody->GetPosition().x, newPlayerBody->GetPosition().y);
+		//move view along w/ player, with an offset on the Y plane.
 		view.move(0, -2 * 50);
 
+		//set the view
 		window.setView(view);
+		//set the clear color
 		window.clear(sf::Color::White);
+		//draw tiles
 		for (auto& tile : tiles)
 		{
 			window.draw(tile);
 		}
+		//draw player
 		window.draw(newPlayer->drawable(newPlayerBody));
+		//display everything
 		window.display();
 	}
-
+	//delete newPlayer object.
 	delete newPlayer;
-	
+
 	return 0;
 }
