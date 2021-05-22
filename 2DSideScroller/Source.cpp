52aff3d8d4a898a9d5417980178fb2a7479481c1
johuad <43306406+johuad@users.noreply.github.com>
2021-05-22 01:58:34 -0500

    Major refactoring & redesign. Added gamestates. Not perfect yet but better than before.

--- 2DSideScroller/Source.cpp
+++ 2DSideScroller/Source.cpp
@@ -1,9 +1,11 @@
 #include "Game.h"
 #include "GameStateMenu.h"
+#include <memory>
 
 int main()
 {
 	//TODO: Implement settings & saving/loading settings from file.
+	//TODO: I still want to do this.
 
 	//set up initial game screen.
 	Game *game = new Game(1280, 700);
